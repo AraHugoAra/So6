f46fe1ad8b6e15096d5407a2c4ce9a1f0f745737
Hugo Aragones <aragones.hugo@gmail.com>
2023-01-18 16:30:59 +0100

    sass + server.js + routes

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express, { json, urlencoded } from 'express'
+import dotenv from 'dotenv'
+import cors from "cors"
+import { createConnection } from 'promise-mysql'
+import usersRoutes from './routes/usersRoutes.js'
+dotenv.config()
+
+const port = process.env.PORT
+const app = express()
+
+app.use(cors())
+app.use(json())
+app.use(urlencoded({extended: false}))
+
+const connectionOptions = {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.PW_DB,
+    port: process.env.DB_PORT
+}
+
+createConnection(connectionOptions)
+    .then(async (db) => {
+        app.get('/', async (req, res) => {
+            try {
+                res.json({status: 200, msg: "C'est OK, c'est BAT, c'est IN."})
+            }
+            catch(error) {
+                res.send(error)
+            }
+        })
+        usersRoutes(app, db)
+    })
+
+app.listen(port, () => {console.log(`🏃💨 Server is running on port ${port}`)})
