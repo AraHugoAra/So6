c42b2aee8581c9f36e5a569394a5f0a1c26c0819
Hugo Aragones <aragones.hugo@gmail.com>
2023-01-19 15:29:13 +0100

    authenticated post POST route

--- backend/routes/commentsRoutes.js
+++ backend/routes/commentsRoutes.js
@@ -15,7 +15,6 @@ function commentsRoutes(app, db)  {
             res.send(error)
         }
     })
-
     // get all with user id
     // SHOW
     app.get('/comments/:id', async (req, res) => {
@@ -30,21 +29,6 @@ function commentsRoutes(app, db)  {
             res.json(error)
         }
     })
-    // // UPDATE
-    // app.put('/categories/:catId', async (req, res) => {
-    //     const catId = req.params.catId
-    //     const newName = req.body.name
-    //     try{
-    //         const respDB = await db.query(
-    //             'UPDATE categories SET name = ? WHERE id = ?'
-    //             ,[newName, catId])
-    //         res.json({status: 200, respDB})
-    //     }
-    //     catch(error) {
-    //         res.json(error)
-    //     }
-    // })
-
     // CREATE quel chemin ??
     app.post('/comments', async (req, res) => {
         // const body = req.body.body
@@ -67,32 +51,6 @@ function commentsRoutes(app, db)  {
             res.json(error)
         }
     })
-    // // DELETE
-    // app.delete('/categories/:catId', async (req, res) => {
-    //     const catId = req.params.catId
-    //     try{
-    //         const respDB = await db.query(
-    //             'DELETE FROM categories WHERE id = ?'
-    //             ,[catId])
-    //             res.json({status: 200, respDB})
-    //     }
-    //     catch(error){
-    //         res.json(error)
-    //     }
-    //     })
-    // // ADDITIONAL
-    // app.get('/categories/:category_name/toys', async (req, res) => {
-    //     const catName = req.params.category_name
-    //     try{
-    //         const respDB = await db.query(
-    //             'SELECT toys.name, toys.id, toys.description, toys.price, categories.name AS category FROM toys INNER JOIN categories ON toys.category = categories.id WHERE categories.name = ?'
-    //         , [catName])
-    //         res.json({status: 200, respDB})
-    //     }
-    //     catch(error){
-    //         res.json(error)
-    //     }
-    // })
-    }
+}
 
 export default commentsRoutes
