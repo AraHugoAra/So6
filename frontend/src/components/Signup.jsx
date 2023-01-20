f2a3da84ba087af003bd5bfe8ae54583456e285a
Hugo Aragones <aragones.hugo@gmail.com>
2023-01-20 12:00:27 +0100

    Signup test push

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,5 +1,8 @@
 export default function Signup() {
     return(
-        <p>Signup</p>
+        <form>
+            <label htmlFor="email">email</label>
+            <input type="email" id="email"/>
+        </form>
     )
 }
