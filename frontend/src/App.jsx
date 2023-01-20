b0d878704a439090b73faf75c27dc2a5b27658c5
Laurent Binder <laurentbinder@gmail.com>
2023-01-20 16:40:56 +0100

    remove commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,16 @@ import { Route, Routes } from 'react-router'
 import Signup from './components/Signup'
 import Landing from './components/Landing'
 import './sass/main.scss'
+import Login from './components/Login'
 
 function App() {
   return (
     <div>
       <Routes>
+        <p>app ici</p>
         <Route path="/" element={<Landing />} />
-        <Route path="/signup" element={<Signup />} />
+        {/* <Login /> */}
+        {/* <Route path="/signup" element={<Signup />} /> */}
       </Routes>
     </div>
   )
