ee051f8369e10e58f616d5cb79a41069229995e7
HugoA <aragones.hugo@gmail.com>
2023-01-23 15:18:46 +0100

    login + signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router'
 import Signup from './components/Signup'
+import Footer from './components/Footer'
 import Landing from './components/Landing'
 import './sass/main.scss'
 import Login from './components/Login'
@@ -11,13 +12,13 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+        
+        <Route path="/" element={<><Navbar /><Landing /></>} />
+        <Route path="/signup" element={<><Signup /><Footer /></>} />
+        <Route path="/login" element={<><Login /><Footer /></>} />
         <Route path="/logout" element={<Logout />} />
       </Routes>
-        <Navbar />
-        <UserProfile />
+        {/* <UserProfile /> */}
     </div>
   )
 }
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,16 +1,30 @@
-import { Link } from 'react-router-dom'
+import { Link, redirect, useNavigate } from 'react-router-dom'
+import logo from '../assets/icons/So6_logo_light.svg'
 
 export default function Login() {
-    const baseUrl = import.meta.env.VITE_BASE_URL
 
-    async function postFetch(body) {
-        await fetch(`${import.meta.env.VITE_BASE_URL}/login`, {
-            method: 'POST',
-            credentials: 'include',
-            headers: { "Content-Type": "application/json",
-         },
-            body: JSON.stringify(body),
-        })
+    const navigate = useNavigate()
+
+    async function postFetch(e, body) {
+        try {
+            const response = await fetch(`${import.meta.env.VITE_BASE_URL}/login`, {
+                method: 'POST',
+                credentials: 'include',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(body),
+            })
+            const json = await response.json()
+            if (json.status === 200) {
+                e.target.reset()
+                navigate('/')
+            }
+            else {
+                alert('Vérifiez vos identifiants.')
+            }
+        }
+        catch(error) {
+            console.log('Login post error: ', error)
+        }
     }
 
     function handleSubmit(e) {
@@ -19,29 +33,23 @@ export default function Login() {
             email: e.target.elements.email.value,
             password: e.target.elements.password.value
         }
-        postFetch(body)
-        // e.target.reset()
-    }
-
-    async function handleTest() {
-        const resp = await fetch(`${import.meta.env.VITE_BASE_URL}/test`, {
-            method: 'GET',
-            credentials: 'include',
-        })
-        // console.log(resp)
+        postFetch(e, body)
     }
 
     return(
-        <div>
+        <div className="container login">
+            <div className="login__logo">
+                <img src={logo} alt="so6-logo" />
+                <p className='login__flavour-text' >Inscrivez-vous pour voir les photos et vidéos de vos amis.</p>
+            </div>
             <form onSubmit={(e) => handleSubmit(e)}>
-                <label htmlFor="email">Email</label>
+                <label htmlFor="email">Email:</label>
                 <input type="email" id="email"/>
-                <label htmlFor="password">Password</label>
+                <label htmlFor="password">Password:</label>
                 <input type="password" id="password"/>
                 <button type="submit">Se connecter</button>
             </form>
-            <button onClick={() => handleTest()}>Test Login</button>
-            <Link to="/logout">Logout</Link>
+            <Link className='form__link' to="/signup">Inscrivez-vous.</Link>
         </div>
     )
 }
