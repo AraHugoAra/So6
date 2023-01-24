7f74a4d9e87eaac0d3d52ff962893a9f9aa2dcd9
Laurent Binder <laurentbinder@gmail.com>
2023-01-24 15:36:50 +0100

    add navbars and modif in feed and scss

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -8,36 +8,41 @@ export default function Feed({}) {
 
     const { data, loading, error } = useFetch('posts', {}, [])
 
-    return(
-        <div className="feed">
-            {(!loading && !error && data.posts) && data.posts.map((post) => 
-                <div 
-                  className="post"
-                  key={post.id}>
-                    <div 
-                      className="post__user">
-                        <img className="post__user__avatar avatar" src={post.avatar || "./../../public/favicon.ico"}/>
-                        <div  className="post__user__nickname">{post.nickname}</div>
-                    </div>
-                    <div 
-                      className="post__media">
-                        <img src={post.media}/>
-                    </div>
-                    <div 
-                      className="post__buttons">
-                        <div
-                          className="post__buttons__like">
-                            <Like target_id={post.id} target_type={0}/>
-                        </div>
-                        <img src={Comment}/>
-                    </div>
-                    <div 
-                      className="post__text">
-                        <p className="post__text body">{post.body}</p>
-                        <p className="post__text comments">Voir les commentaires...</p>
-                    </div>
+    return (
+      <div className="feed">
+        {!loading &&
+          !error &&
+          data.posts &&
+          data.posts.map((post) => (
+            <div className="post" key={post.id}>
+              <div className="post__user">
+                <img
+                  className="post__user__avatar avatar"
+                  src={post.avatar || "./../../public/favicon.ico"}
+                />
+                <div className="post__user__text">
+                  <p className="post__user__text__nickname">{post.nickname}</p>
+                  <p className="post__user__text__body body">{post.body}</p>
                 </div>
-            )}
-        </div>
-    )
+              </div>
+              <div className="post__media">
+                <img src={post.media} />
+              </div>
+              <div className="post__buttons">
+                <div className="post__buttons__like">
+                  <Like target_id={post.id} target_type={0} />
+                </div>
+                <button>
+                  <img src={Comment} alt='comment-icon'/>
+                </button>
+              </div>
+              <div className="post__comments">
+                <p className="post__comments comments">
+                  Voir les commentaires...
+                </p>
+              </div>
+            </div>
+          ))}
+      </div>
+    );
 }
---
+++ frontend/src/components/NavbarTop.jsx
@@ -0,0 +1,37 @@
+import React, { useReducer } from 'react';
+import Logo from "./../assets/icons/So6_logo_light.svg";
+import Chat from "./../assets/icons/chat-light-mode.svg";
+import Like from "./../assets/icons/Like-false-light.png";
+import Add from "./../assets/icons/Add_light.svg";
+import Logout from './Logout';
+
+const NavbarTop = () => {
+  return (
+
+    <div className='navbar-top'>
+      <div className="navbar navbar-expand-sm navbar-light">
+        <div className="navbar__container">
+          <a className="navbar__logo" href="/">
+            <img src={Logo} alt="brand-icon" />
+          </a>
+          <div className="navbar__icons">
+            <a className="navbar__logo" href="/">
+              <img src={Like} alt="like-icon" />
+            </a>
+            {/* //chat with a friend  */}
+            <a className="navbar__logo" href="/">
+              <img src={Chat} alt="chat-icon" />
+            </a>
+            <a className="navbar__logo" href="/">
+              <img src={Add} alt="add-icon" />
+            </a>
+            <Logout />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    );
+}
+
+export default NavbarTop;
