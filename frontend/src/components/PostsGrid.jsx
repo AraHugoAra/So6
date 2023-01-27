62c6d117a7399e3e580dd78ff636b6d3fda9151d
nelisa <ntl.elisa@gmail.com>
2023-01-27 16:00:04 +0100

    add postdetails from user page

--- frontend/src/components/PostsGrid.jsx
+++ frontend/src/components/PostsGrid.jsx
@@ -4,11 +4,19 @@ import useFetch from '../hooks/useFetch';
 import HandleImageError from '../services/HandleImageError';
 import Avatar from './Avatar';
 import UserProfile from './UserProfile';
+import PostDetail from './PostDetail';
 
 const PostsGrid = () => {
+    const [modalIsOpen, setIsOpen] = useState(false);
     const { id } = useParams()
     const { data, loading, error } = useFetch(`users/${id}/posts`, null, [window.location.href]);
 
+    function openModal(post_id) {
+      setIsOpen(post_id);
+    }
+    function closeModal() {
+      setIsOpen(null);
+    }
     return (
       <>
         {!loading && !error && data.status === 200 && (
@@ -22,12 +30,20 @@ const PostsGrid = () => {
           <div className="posts-grid__row-images">
             {!loading && !error &&
               data.posts.map((post) => (
-                  <img
-                    key={post.id}
-                    className="posts-grid__image"
-                    src={post.media}
-                    alt={"image n°" + post.id}
-                  />
+                <>
+                <img
+                  key={post.id}
+                  className="posts-grid__image"
+                  src={post.media}
+                  alt={"image n°" + post.id}
+                  onClick={(e) => openModal(post.id)}
+                />
+                <PostDetail
+                  postId={modalIsOpen}
+                  modalIsOpen={post.id===modalIsOpen}
+                  closeModal={closeModal}
+                />
+              </>
               ))}
           </div>
         </article>
