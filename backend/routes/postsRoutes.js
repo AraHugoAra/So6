ebf06592d00a3b3e0e25c73e8865145d009ac529
Elisa <ntl.elisa@gmail.com>
2023-02-24 14:34:17 +0100

    some posts route test

--- backend/routes/postsRoutes.js
+++ backend/routes/postsRoutes.js
@@ -1,72 +1,71 @@
-import isAuthenticated from '../middleware/isAuthenticated.js'
+import isAuthenticated from "../middleware/isAuthenticated.js";
 
-function postsRoutes(app, db)  {
-    // GET posts for home page
-    app.get('/posts', async (req, res) => {
-        try {
-            const posts = await db.query(
-                "SELECT posts.id,posts.timestamp,posts.media,posts.body,posts.vegan, posts.user_id,users.nickname, users.avatar , COUNT(likes.user_id) as number_of_like "+
-                "FROM posts " +
-                "INNER JOIN users ON users.id = posts.user_id "+
-                "LEFT JOIN likes ON posts.id = likes.target_id AND likes.target_type=0 "+
-                "GROUP BY posts.id "+
-                "ORDER BY posts.timestamp DESC")
-            res.json({status: 200, posts})
-        }
-        catch(error) {
-            res.send(error)
-        }
-    })
+function postsRoutes(app, db) {
+  // GET posts for home page
+  app.get("/posts", async (req, res) => {
+    try {
+      const posts = await db.query(
+        "SELECT posts.id,posts.timestamp,posts.media,posts.body,posts.vegan, posts.user_id,users.nickname, users.avatar , COUNT(likes.user_id) as number_of_like " +
+          "FROM posts " +
+          "INNER JOIN users ON users.id = posts.user_id " +
+          "LEFT JOIN likes ON posts.id = likes.target_id AND likes.target_type=0 " +
+          "GROUP BY posts.id " +
+          "ORDER BY posts.timestamp DESC"
+      );
+      res.json({ status: 200, posts });
+    } catch (error) {
+      res.send(error);
+    }
+  });
 
-    // GET post for details
-    app.get('/posts/:post_id', async (req, res) => {
-        const post_id = req.params.post_id
-        try {
-            const post = await db.query(
-                "SELECT posts.id,posts.timestamp,posts.media,posts.body,posts.user_id,posts.vegan,users.nickname, users.avatar "+
-                "FROM posts " +
-                "INNER JOIN users ON users.id = posts.user_id " +
-                "WHERE posts.id= ?",
-                [post_id]
-                )
-            res.json({status: 200, post})
-        }
-        catch(error) {
-            res.send(error)
-        }
-    })
+  // GET post for details
+  app.get("/posts/:post_id", async (req, res) => {
+    const post_id = req.params.post_id;
+    try {
+      const post = await db.query(
+        "SELECT posts.id,posts.timestamp,posts.media,posts.body,posts.user_id,posts.vegan,users.nickname, users.avatar " +
+          "FROM posts " +
+          "INNER JOIN users ON users.id = posts.user_id " +
+          "WHERE posts.id= ?",
+        [post_id]
+      );
+      post.length ? res.json({ status: 200, post }) : res.sendStatus(404);
+    } catch (error) {
+      res.send(error);
+    }
+  });
 
-    // GET posts for user profil
-    app.get('/users/:user_id/posts', async (req, res) => {
-        const user_id = req.params.user_id
-        try {
-            const posts = await db.query(
-                "SELECT posts.id,posts.timestamp,posts.media,posts.user_id, users.avatar, users.nickname "+
-                "FROM posts " +
-                "RIGHT JOIN users ON users.id = posts.user_id WHERE users.id= (?) "+
-                "ORDER BY posts.timestamp DESC",[user_id])
-            res.json({status: 200, posts})
-        }
-        catch(error) {
-            res.send(error)
-        }
-    })
+  // GET posts for user profil
+  app.get("/users/:user_id/posts", async (req, res) => {
+    const user_id = req.params.user_id;
+    try {
+      const posts = await db.query(
+        "SELECT posts.id,posts.timestamp,posts.media,posts.user_id, users.avatar, users.nickname " +
+          "FROM posts " +
+          "RIGHT JOIN users ON users.id = posts.user_id WHERE users.id= (?) " +
+          "ORDER BY posts.timestamp DESC",
+        [user_id]
+      );
+      posts.length ? res.json({ status: 200, posts }) : res.sendStatus(404);
+    } catch (error) {
+      res.send(error);
+    }
+  });
 
-    // POST post
-    app.post('/newpost', isAuthenticated, async (req,res) => {
-        const user_id = req.session.user[0].id
-        const { media, body, vegan = false } = {...req.body}
-        try{
-            const responseDB = await db.query(
-                "INSERT INTO posts (media, body, user_id, vegan) VALUES (?,?,?,?)",
-                [media, body, user_id, vegan]
-              );
-              res.json({ status: 200, responseDB })
-        }
-        catch(error) {
-            res.send(error)
-        }
-    })
+  // POST post
+  app.post("/newpost", isAuthenticated, async (req, res) => {
+    const user_id = req.session.user[0].id;
+    const { media, body, vegan = false } = { ...req.body };
+    try {
+      const responseDB = await db.query(
+        "INSERT INTO posts (media, body, user_id, vegan) VALUES (?,?,?,?)",
+        [media, body, user_id, vegan]
+      );
+      res.json({ status: 200, responseDB });
+    } catch (error) {
+      res.send(error);
+    }
+  });
 }
 
-export default postsRoutes
+export default postsRoutes;
--- backend/tests/test.js
+++ backend/tests/test.js
@@ -22,8 +22,48 @@ describe("Test the root path", () => {
 });
 
 describe("Test the users path", () => {
-  it("should send an array of users", async () => { 
-    const res = await request(app).get('/users');
+  it("should send an array of users", async () => {
+    const res = await request(app).get("/users");
     expect(res.body.users).toHaveLength(5);
-  })
-})
+  });
+});
+
+describe("Test the posts path", () => {
+  it("should send an array of posts", async () => {
+    const res = await request(app).get("/posts");
+    expect(res.body.posts).toHaveLength(11);
+  });
+
+  it("should send the post when id exists", async () => {
+    const res = await request(app).get("/posts/9");
+    expect(res.body.post).toHaveLength(1);
+    expect(res.body.post[0]).toEqual({
+      id: 9,
+      timestamp: "2023-01-29T16:31:37.000Z",
+      media:
+        "https://i.pinimg.com/736x/e1/fa/43/e1fa4375327f1f3519c3eba7cff8a31a.jpg",
+      body: "Enfin un réseau saucisssse !!",
+      user_id: 11,
+      vegan: 0,
+      nickname: "Saucisse Addictive 47",
+      avatar:
+        "http://res.cloudinary.com/drrlu6nxb/image/upload/v1675009194/user_images/ouw46mjtjh83aqjhpskj.png",
+    });
+  });
+
+  it("should send an error when id doesn't exist", async () => {
+    const res = await request(app).get("/posts/1");
+    expect(res.statusCode).toEqual(404);
+  });
+
+  it("should send posts when user_id exists", async () => {
+    const res = await request(app).get("/users/11/posts");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.posts).toHaveLength(6)
+  });
+
+  it("should send an error when user_id doesn't exist", async () => {
+    const res = await request(app).get("/users/1/posts");
+    expect(res.statusCode).toEqual(404);
+  });
+});
